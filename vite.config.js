fa0ae7d81e93cb38b95fba154c30f6dd8ee222b6
musharaf4335 <shaikmusharaf@integramicro.co.in>
2024-06-17 16:07:38 +0530

    Resolve merge conflicts

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/swathi_digital_photos/', // Replace with your repository name
   plugins: [react()],
   build: {
     outDir: 'dist'
   },
-//  base: '/swathi_digital_photos/', // Ensure the base path is set to your repository name
   assetsInclude: ['**/*.JPG', '**/*.jpg']
 })
